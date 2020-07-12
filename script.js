0f34f83f2dc69bf51cb1a3df50ae7a798a3ff14c
Shifatur-Rahman <rjnovel28@gmail.com>
2020-07-12 19:32:36 +0600

    es5 features

--- script.js
+++ script.js
@@ -1,90 +1,32 @@
-// contact constructor
-function Contact(name, email, phone, birthDate) {
+function Contact(name, email, phone, birthDay) {
   this.name = name;
   this.email = email;
   this.phone = phone;
-  this.birthDate = birthDate;
+  this.birthday = birthday;
 }
-
-//UI Constructor
+// prototype class
 function UI() {}
-
-//add new contact
-UI.prototype.addContactToList = function (contact) {
-  var list = document.querySelector("#contact-list");
-  var tr = document.querySelector(".tr");
-  tr.innerHTML = `
-    
-    <td>${contact.name} </td>
-    <td>${contact.email} </td>
-    <td>${contact.phone} </td>
-    <td>${contact.birthDate} </td>
-    <td> <a class="btn btn-floating delete">X</a> </td>
-    `;
-  list.appendChild(tr);
+// add contact
+UI.prototype.add = function () {};
+// delete contact
+UI.prototype.delete = function () {};
+// search Contact
+UI.prototype.search = function () {};
+// clear contact
+UI.prototype.clear = function () {
+  document.querySelector("#name").value = " ";
+  document.querySelector("#email").value = " ";
+  document.querySelector("#phone").value = " ";
+  document.querySelector("#birth").value = " ";
 };
 
-// delete btn
-UI.prototype.deleteContactFormList = function (target) {
-  if (target.classList.contains("delete")) {
-    if (confirm("Are You Sure")) {
-      target.parentElement.parentElement.remove();
-    }
-  }
-};
-//search
-UI.prototype.searchContact = function (search) {
-  var allName = document.querySelectorAll("#contact-list tr");
-  allName.forEach((rows) => {
-    if (rows.children[0].textContent.indexOf(search) != -1) {
-      rows.style.display = "table-row";
-    } else {
-      rows.style.display = "none";
-    }
-  });
-};
+//   Events
 
-UI.prototype.showAlert = function () {};
-// after submit clear input field
-UI.prototype.clearFields = function (e) {
-  document.getElementById("fullName").value = "";
-  document.getElementById("email").value = "";
-  document.getElementById("phone").value = "";
-  document.getElementById("birth").value = "";
-};
-/*                             Events                  */
-//submit button
-document
-  .getElementById("contact-form")
-  .addEventListener("submit", function (e) {
-    e.preventDefault();
-    const name = document.getElementById("fullName").value;
-    const email = document.getElementById("email").value;
-    const phone = document.getElementById("phone").value;
-    const birthDate = document.getElementById("birth").value;
-
-    const contact = new Contact(name, email, phone, birthDate);
-
-    const ui = new UI();
-
-    if (name == "" || email === "" || phone === "" || birthDate === "") {
-      console.log("ERROR");
-    } else {
-      ui.addContactToList(contact);
-      ui.clearFields();
-    }
-  });
-
-// delete button
-document.querySelector("#contact-list").addEventListener("click", function (e) {
+document.querySelector(".form-input").addEventListener("submit", function (e) {
   e.preventDefault();
-  const ui = new UI();
-  ui.deleteContactFormList(e.target);
-});
-
-// search event
-
-document.querySelector("#filter").addEventListener("keyup", function (e) {
-  const ui = new UI();
-  ui.searchContact(e.target.value);
+  var name = document.querySelector("#name").value;
+  var email = document.querySelector("#email").value;
+  var phone = document.querySelector("#phone").value;
+  var birth = document.querySelector("#birth").value;
+  var contactList = document.querySelector(".tableList");
 });
